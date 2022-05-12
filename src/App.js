ee8dc0fb29652a4caaeb44e2c4ca254fc157e7b7
Md-Mobassher <you@example.com>
2022-05-12 13:06:33 +0600

    Navbar and banner added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Pages/About/About';
+import Appoinment from './Pages/Appointment/Appoinment';
+import ContactUs from './Pages/Contact Us/ContactUs';
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import Reviews from './Pages/Reviews/Reviews';
+import Navbar from './Shared/Navbar';
+import NotFound from './Shared/NotFound';
 
 function App() {
   return (
-    <div >
-      <button class="btn btn-primary">Button</button>
-      <button class="btn btn-secondary">Button</button>
-      <button class="btn btn-accent">Button</button>
+    <div className='px-12 mx-auto  ' >
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={< Home />}></Route>
+        <Route path='appoinment' element={<Appoinment />}></Route>
+        <Route path='reviews' element={<Reviews />}></Route>
+        <Route path='about' element={<About />}></Route>
+        <Route path='contact' element={<ContactUs />}></Route>
+        <Route path='login' element={<Login />}></Route>
+        <Route path='*' element={<NotFound />}></Route>
+      </Routes>
+      
     </div>
   );
 }
